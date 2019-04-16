7fe3ad4fa4339c46961b29ade3e7429104e8c036
Ethan Zimmerman <thebinarypenguin@gmail.com>
2019-04-16 13:33:51 -0400

    Move code from DashboardRoute to Dashboard

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+import config from '../../config'
+import TokenService from '../../services/token-service'
+
+import './Dashboard.css'
+
+function WordList (props) {
+
+  const rows = props.data.map((w) => {
+    return (
+      <li key={w.id}>
+        <h4>{w.original}</h4>
+        <p>correct answer count: <span className="correct">{w.correct_count}</span></p>
+        <p>incorrect answer count: <span className="incorrect">{w.incorrect_count}</span></p>
+      </li>
+    );
+  });
+
+  return (
+    <ul className="WordList">{ rows }</ul>
+  );
+}
+
+
+class Dashboard extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      language : {},
+      words    : [],
+      error    : null,
+    };
+  }
+
+  componentDidMount() {
+
+    fetch(`${config.API_ENDPOINT}/language`, {
+      headers: {
+        'authorization': `Bearer ${TokenService.getAuthToken()}`,
+      }
+    })
+    .then((res) => {
+
+      if (!res.ok) {
+        return res.json().then(e => Promise.reject(e))
+      }
+
+      return res.json();
+    })
+    .then((json) => {
+      this.setState({
+        language: json.language,
+        words: json.words,
+      });
+    })
+    .catch(res => {
+      this.setState({ error: res.error })
+    });
+  }
+
+  render() {
+    return (
+      <section id="Dashboard">
+
+        <h2>{this.state.language.name}</h2>
+
+        <p>Total correct answers: {this.state.language.total_score}</p>
+
+        <h3>Words to practice</h3>
+        <WordList data={this.state.words} />
+
+        <Link to="/learn">Start practicing</Link>
+      </section>
+    );
+  }
+}
+
+export default Dashboard
