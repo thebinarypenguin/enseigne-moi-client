9e0473370918faecf63118554148fe51976401c2
Ethan Zimmerman <thebinarypenguin@gmail.com>
2019-05-20 11:51:25 -0400

    Rename to Enseigne Moi

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -43,7 +43,7 @@ class Header extends Component {
       <header>
         <h1>
           <Link to='/'>
-            Spaced repetition
+            Enseigne Moi
           </Link>
         </h1>
         {TokenService.hasAuthToken()
